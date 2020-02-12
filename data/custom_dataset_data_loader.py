b60b916ec8c540a22ea6487fc44cdffc85e3f799
Vo Trung Nhan <1612372@hcmut.edu.vn>
2020-02-12 23:50:51 +0700

    add mri dataset

--- data/custom_dataset_data_loader.py
+++ data/custom_dataset_data_loader.py
@@ -4,8 +4,9 @@ from data.base_data_loader import BaseDataLoader
 
 def CreateDataset(opt):
     dataset = None
-    from data.aligned_dataset import AlignedDataset
-    dataset = AlignedDataset()
+    # from data.aligned_dataset import AlignedDataset
+    from data.mri_dataset import MRI_Dataset
+    dataset = MRI_Dataset()
 
     print("dataset [%s] was created" % (dataset.name()))
     dataset.initialize(opt)
